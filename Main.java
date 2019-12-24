2e280a15027fe42e14d4bef92019ac6d2427be4c
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2019-12-24 06:02:13 +0200

    Working image quantization

--- Main.java
+++ Main.java
@@ -1,4 +1,68 @@
+import java.awt.image.BufferedImage;
+import java.awt.Color;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+
 class Main{
     public static void main(String[] args){
+        //VectorQuantizer.Matrix mat1 = new VectorQuantizer.Matrix(6, 6);
+        //int[][] data = {
+            //{1,2,7,9,4,11},
+            //{3,4,6,6,12,12},
+            //{4,9,15,14,9,9},
+            //{10,10,20,18,8,8},
+            //{4,3,17,16,1,4},
+            //{4,5,18,18,5,6},
+        //};
+        //for(int i = 0; i < mat1.rows; ++i){
+            //for(int j = 0; j < mat1.cols; ++j){
+                //mat1.data[i][j] = data[i][j];
+            //}
+        //}
+        //try {
+            //VectorQuantizer.QuantizedData q = VectorQuantizer.quantize(mat1, 2, 2, 4);
+            //for (int i = 0; i < q.data.length; i++) {
+                //for (int j = 0; j < q.data[i].length; j++) {
+                    //System.out.print(q.data[i][j] + " ");
+                //}
+                //System.out.print("\n");
+            //}
+        //} catch (Exception e) {
+            //// TODO Auto-generated catch block
+            //e.printStackTrace();
+        //}
+        try {
+            final int blockWidth = 10;
+            final int blockHeight = 10;
+            final int codeBook = 10;
+            BufferedImage input = ImageIO.read(new File("./peng.bmp"));
+            BufferedImage output = new BufferedImage(input.getWidth(), input.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+            VectorQuantizer.Matrix mat = new VectorQuantizer.Matrix(input.getWidth(), input.getHeight());
+            // Put image in matrix
+            for (int i = 0; i < input.getWidth(); i++) {
+                for (int j = 0; j < input.getHeight(); j++) {
+                    final int color = input.getRGB(i, j);
+                    final int red = (color &   0x00ff0000) >> 16;
+                    final int green = (color & 0x0000ff00) >> 8;
+                    final int blue = (color &  0x000000ff);
+                    mat.data[i][j] = (red+green+blue) / 3;
+                }
+            }
+            VectorQuantizer.QuantizedData q = VectorQuantizer.quantize(mat, blockWidth, blockHeight, codeBook);
+            for (int i = 0; i < input.getWidth(); i++) {
+                for (int j = 0; j < input.getHeight(); j++) {
+                    final int ii = i / blockHeight;
+                    final int jj = j / blockWidth;
+                    VectorQuantizer.Matrix block = q.codebook[q.data[ii][jj]];
+                    final int color = (int) block.data[i%blockHeight][j%blockWidth];
+                    output.setRGB(i, j, new Color(color, color, color).getRGB());
+                }
+            }
+            ImageIO.write(output, "bmp", new File("output.bmp"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- VectorQuantizer.java
+++ VectorQuantizer.java
@@ -1,31 +1,92 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class VectorQuantizer{
     static class Matrix{
         public double[][] data;
-        int n, m;
-        Matrix(int n, int m){
-            this.n = n;
-            this.m = m;
-            this.data = new double[n][m];
+        int rows, cols;
+        Matrix(int rows, int cols){
+            this.rows = rows;
+            this.cols = cols;
+            this.data = new double[rows][cols];
         }
         void print(){
-            for(int i = 0; i < this.n; ++i){
-                for(int j = 0; j < this.n; ++j){
-                    System.out.print(this.data[n][m] + " ");
+            for(int i = 0; i < this.rows; ++i){
+                for(int j = 0; j < this.cols; ++j){
+                    System.out.print(this.data[i][j] + " ");
                 }
                 System.out.print("\n");
             }
         }
+        Matrix[] divide(int blockWidth, int blockHeight) throws Exception{
+            if(this.rows % blockHeight != 0) throw new Exception("Cannot divide matrix Vertically");
+            if(this.cols % blockWidth != 0) throw new Exception("Cannot divide matrix Horizontally");
+            final int blocksPerRow = cols/blockWidth;
+            final int blocksPerCol = rows/blockHeight;
+            Matrix[] matricies = new Matrix[blocksPerRow * blocksPerCol];
+            for(int i = 0; i < this.rows; ++i){
+                for(int j = 0; j < this.cols; ++j){
+                    // find matrix index, i, j for submatrix
+                    final int idx = ((i / blockHeight) * blocksPerRow) + (j/blockWidth); 
+                    if(matricies[idx] == null)
+                        matricies[idx] = new Matrix(blockHeight, blockWidth);
+                    final int sub_i = i % blockHeight;
+                    final int sub_j = j % blockWidth;
+                    matricies[idx].data[sub_i][sub_j] = this.data[i][j];
+                }
+            }
+            return matricies;
+        }
+    }
+
+    static class QuantizedData{
+        Matrix[] codebook;
+        int[][] data;
+    }
+
+    public static QuantizedData quantize(Matrix original, int blockWidth, int blockHeight, int codebookSize) throws Exception{
+        QuantizedData output = new QuantizedData();
+        Matrix[] blocks = original.divide(blockWidth, blockHeight);
+        output.data = new int[original.rows/blockHeight][original.cols/blockWidth];
+        output.codebook = kMeans(blocks, codebookSize);
+        for(int i = 0; i < blocks.length; ++i){
+            final int idI = i / (original.cols / blockWidth);
+            final int idJ = i % (original.cols / blockWidth);
+            output.data[idI][idJ] = getClosestMatrixIdx(output.codebook, blocks[i]);
+        }
+        return output;
+    }
+
+    public static double mse(Matrix a, Matrix target){
+        double mse = 0;
+        for(int i = 0; i < a.rows; ++i){
+            for(int j = 0; j < a.cols; ++j){
+                mse += Math.pow(a.data[i][j] - target.data[i][j], 2);
+            }
+        }
+        return mse;
+    }
+
+    public static int getClosestMatrixIdx(Matrix[] pool, Matrix target){
+        double min = Double.POSITIVE_INFINITY;
+        int minIdx = -1;
+        for(int i = 0; i < pool.length; ++i){
+            double mse = mse(target, pool[i]);
+            if(mse < min){
+                min = mse;
+                minIdx = i;
+            }
+        }
+        return minIdx;
     }
 
     public static Matrix getAverageMatrix(Matrix[] matricies){
         // find average
-        final Matrix avg = new Matrix(matricies[0].n, matricies[0].m);
+        final Matrix avg = new Matrix(matricies[0].rows, matricies[0].cols);
         for(int i = 0; i < matricies.length; ++i){
-            for(int j = 0; j < matricies[i].n; ++j){
-                for(int l = 0; l < matricies[i].m; ++l){
-                    avg.data[j][l] += matricies[i].data[j][l] / matricies.length;
+            for(int a = 0; a < matricies[i].rows; ++a){
+                for(int b = 0; b < matricies[i].cols; ++b){
+                    avg.data[a][b] += matricies[i].data[a][b] / matricies.length;
                 }
             }
         }
@@ -36,37 +97,24 @@ class VectorQuantizer{
         Matrix[] refined = new Matrix[means.length];
         int[] refined_len = new int[means.length];
         for(int i = 0; i < refined.length; ++i){
-            refined[i] = new Matrix(means[i].n, means[i].m);
+            refined[i] = new Matrix(means[i].rows, means[i].cols);
         }
         // Cluster
         for(int i = 0; i < matricies.length; ++i){
-            // Minimize MSE
-            int min = Integer.MAX_VALUE;
-            int minIdx = -1;
-            for(int j = 0; j < means.length; ++j){
-                int mse = 0;
-                for(int a = 0; a < means[j].n; ++a){
-                    for(int b = 0; b < means[i].n; ++b){
-                        mse += Math.pow(matricies[i].data[a][b] - means[j].data[a][b], 2);
-                    }
-                }
-                if(mse < min){
-                    min = mse;
-                    minIdx = j;
-                }
-            }
-            for(int a = 0; a < refined[minIdx].n; ++a){
-                for(int b = 0; b < refined[minIdx].n; ++b){
+            int minIdx = getClosestMatrixIdx(means, matricies[i]);
+            refined_len[minIdx]++;
+            for(int a = 0; a < refined[minIdx].rows; ++a){
+                for(int b = 0; b < refined[minIdx].cols; ++b){
                     refined[minIdx].data[a][b] += matricies[i].data[a][b];
-                    refined_len[minIdx]++;
                 }
             }
         }
         // Calculate new averages
         for(int i = 0; i < refined.length; ++i){
-            for(int a = 0; a < refined[i].n; ++a){
-                for(int b = 0; b < refined[i].n; ++b){
-                    refined[i].data[a][b] /= refined_len[i];
+            for(int a = 0; a < refined[i].rows; ++a){
+                for(int b = 0; b < refined[i].cols; ++b){
+                    if(refined_len[i] > 0)
+                        refined[i].data[a][b] /= refined_len[i];
                 }
             }
         }
@@ -83,17 +131,19 @@ class VectorQuantizer{
             // Split and average
             int n = means.size();
             for(int i = 0; i < n; ++i){
-                // replace i and pop back
-                // minus 1 & plus 1 floored
-                Matrix split = new Matrix(means.get(i).n, means.get(i).m);
-                for(int a = 0; a < split.n; ++a){
-                    for(int b = 0; b < split.m; ++b){
+                Matrix split = new Matrix(means.get(i).rows, means.get(i).cols);
+                for(int a = 0; a < split.rows; ++a){
+                    for(int b = 0; b < split.cols; ++b){
                         double value = means.get(i).data[a][b];
                         split.data[a][b] = Math.floor(value) == value ? value - 1 : Math.floor(value);
-                        means.get(i).data[a][b] = Math.floor(value+1);
+                        means.get(i).data[a][b] = Math.floor(Math.floor(value)+1);
                     }
                 }
                 means.add(split);
+                // Refine means
+                Matrix[] tmp = means.toArray(new Matrix[means.size()]);
+                tmp = refineMeans(tmp, matricies);
+                means = new ArrayList<>(Arrays.asList(tmp));
             }
         }
         // Refine averages by clustering
@@ -103,11 +153,7 @@ class VectorQuantizer{
             refined = refineMeans(old, matricies);
             double diff = 0;
             for(int i = 0; i < refined.length; ++i){
-                for(int a = 0; a < refined[i].n; ++a){
-                    for(int b = 0; b < refined[i].n; ++b){
-                        diff += Math.pow(refined[i].data[a][b] - old[i].data[a][b], 2.0);
-                    }
-                }
+                diff += mse(refined[i], old[i]);
             }
             if(diff == 0) break;
             old = refined;
